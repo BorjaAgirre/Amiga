9fb8df6b53f4a8e11e65134dba7406056c8d1a96
Borja <email>
2014-09-27 00:47:05 +0200

    Formulario en horizontal

--- src/Zubietxe/PrincipalBundle/Entity/IndicadoresRepository.php
+++ src/Zubietxe/PrincipalBundle/Entity/IndicadoresRepository.php
@@ -10,21 +10,34 @@ class IndicadoresRepository extends EntityRepository
 
     public function opcionesIndicador()
     {
+ 
+        // Esta parte es por si queremos rellenar el valor 0 de las posibles respuestas de indicadores. 
+        // Otra alternativa puede ser utilizar la opción 'empty_value' en PersonaType.php
+        // Pero no me ha funcionado muy bien. 
+
+        // En primer lugar rellenamos todos los valores 0 de cada desplegable
+        $query = $this->getEntityManager()
+            ->createQuery(
+                'SELECT i FROM ZubietxePrincipalBundle:Indicadores i GROUP BY i.indicador'
+            );
+            $result = $query->getResult();
+            foreach ($result as $row)  {
+                $opcion[$row->getIndicador()][0] = "---";
+            }
+ 
+
+            // Después rellenamos los valores de los desplegables con las respuestas a los indicadores, y los títulos       
         $query = $this->getEntityManager()
             ->createQuery(
                 'SELECT i FROM ZubietxePrincipalBundle:Indicadores i ORDER BY i.indicador, i.valorindicador ASC'
             );
             $result = $query->getResult();
-            $ultimo_ind = "";
             foreach ($result as $row)  {
                 if ($row->getTitulo() == 'i') {
                     $opcion[$row->getIndicador()][$row->getValorIndicador()] =  $row->getTexto();
                 } else {
                     $titulo[$row->getIndicador()] =  $row->getTexto();
                 }
- //               }
- //               $ultimo_ind = $row->getIndicador();
-
             }
             return array($opcion, $titulo); 
     }
